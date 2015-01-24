3bd9a7fd3f970d3e7dba93a4a76761116bd43ac8
billy3321 <billy3321@gmail.com>
2015-01-25 02:19:19 +0800

    add keywords

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 var api = require("express").Router({caseSensitive: true, strict: true});
 var models  = require('../models');
 
-var filterRecord = function(record) {
+var filterRecord = function (record) {
   console.log(record)
   delete record["category_id"];
   delete record["carrier_id"];
@@ -15,19 +15,19 @@ var filterRecord = function(record) {
   record["issue"] = record["issue"]["name"];
   record["language"] = record["language"]["name"];
   record["collector"] = record["collector"]["name"];
-  record["keywords"].forEach(function(keyword, index, keywords) {
+  record["keywords"].forEach(function (keyword, index, keywords) {
     keywords[index] = keyword["name"];
   })
   return record;
 }
 
-var filterRecords = function(records) {
+var filterRecords = function (records) {
   if(!records) {
     records = [];
   } else {
     records = records.toJSON();
   }
-  records.forEach(function(record, index, records) {
+  records.forEach(function (record, index, records) {
     records[index] = filterRecord(record);
   })
   return records;
@@ -43,12 +43,12 @@ api.get('/records/', function (req, res, next) {
   }
   if (!query) {
     if (limit) {
-      models.Record.query(function(qb) {
+      models.Record.query(function (qb) {
         qb.limit(limit).offset(offset)
       })
       .fetchAll({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
-      .then(function(records){
-        models.knex('records').count('*').then(function(ret){
+      .then(function (records){
+        models.knex('records').count('*').then(function (ret){
           res.json({
             status: "success",
             records: filterRecords(records),
@@ -58,8 +58,8 @@ api.get('/records/', function (req, res, next) {
       })
     } else {
       models.Record.fetchAll({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
-      .then(function(records){
-        models.knex('records').count('*').then(function(ret){
+      .then(function (records){
+        models.knex('records').count('*').then(function (ret){
           res.json({
             status: "success",
             records: filterRecords(records),
@@ -71,13 +71,13 @@ api.get('/records/', function (req, res, next) {
   } else {
     query = '%' + query + '%';
     if (limit) {
-      models.Record.query(function(qb) {
+      models.Record.query(function (qb) {
         qb.where('title', 'LIKE', query).orWhere('content', 'LIKE', query).limit(limit).offset(offset)
       })
       .fetchAll({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
-      .then(function(records){
+      .then(function (records){
         console.log('length', records.length);
-        models.knex('records').count('*').then(function(ret){
+        models.knex('records').count('*').then(function (ret){
           res.json({
             status: "success",
             records: filterRecords(records),
@@ -86,13 +86,13 @@ api.get('/records/', function (req, res, next) {
         });
       })
     } else {
-      models.Record.query(function(qb) {
+      models.Record.query(function (qb) {
         qb.where('title', 'LIKE', query).orWhere('content', 'LIKE', query)
       })
       .fetchAll({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
-      .then(function(records){
+      .then(function (records){
         console.log('length', records.length);
-        models.knex('records').count('*').then(function(ret){
+        models.knex('records').count('*').then(function (ret){
           res.json({
             status: "success",
             record: filterRecords(records),
@@ -104,38 +104,112 @@ api.get('/records/', function (req, res, next) {
   }
 });
 api.get('/records/:id/', function (req, res, next) {
-    var id = req.params.id
-    if (isNaN(id)) {
-      new models.Record({identifier: id}).fetch({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
-      .then(function(record) {
-        if(record) {
-          return res.json({
-            status: "success",
-            record: filterRecord(record.toJSON()),
-          });
-        } else {
-          return res.json({
-            status: "failed",
-            error: 'not found'
-          });
-        }
-      })
-    } else {
-      new models.Record({id: id}).fetch({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
-      .then(function(record) {
-        if(record) {
-          return res.json({
-            status: "success",
-            record: filterRecord(record.toJSON()),
-          });
-        } else {
-          return res.json({
-            status: "failed",
-            error: 'not found'
-          });
-        }
-      })
-    }
-  });
+  var id = req.params.id
+  if (isNaN(id)) {
+    new models.Record({identifier: id}).fetch({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
+    .then(function (record) {
+      if(record) {
+        return res.json({
+          status: "success",
+          record: filterRecord(record.toJSON()),
+        });
+      } else {
+        return res.json({
+          status: "failed",
+          error: 'not found'
+        });
+      }
+    })
+  } else {
+    new models.Record({id: id}).fetch({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
+    .then(function (record) {
+      if(record) {
+        return res.json({
+          status: "success",
+          record: filterRecord(record.toJSON()),
+        });
+      } else {
+        return res.json({
+          status: "failed",
+          error: 'not found'
+        });
+      }
+    })
+  }
+});
+
+api.get('/keywords/'), function (req, res, next) {
+  var query = req.params.query;
+  var offset = req.query.offset;
+  var limit = req.query.limit;
+}
+
+api.get('/keywords/:query/', function (req, res, next) {
+  var query = req.params.query;
+  var offset = req.query.offset;
+  var limit = req.query.limit;
+  if (isNaN(query)) {
+    query = '%' + query + '%'
+    models.knex.raw("SELECT keywords_records.record_id \
+      FROM keywords_records INNER JOIN keywords ON keywords_records.keyword_id = keywords.id \
+      WHERE keywords.name LIKE ? \
+      GROUP BY keywords_records.record_id;", [query])
+    .then(function (ret){
+      ret = ret.rows
+      if(!ret) {
+        return res.json({
+          status: "success",
+          record: [],
+          count: 0
+        });
+      } else {
+        ret.forEach(function (record, index, records) {
+          records[index] = record["record_id"];
+        })
+        models.Record.query(function (qb) {
+            qb.where('id', 'in', ret).limit(limit).offset(offset)
+          })
+          .fetchAll({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
+          .then(function (records){
+            return res.json({
+              status: "success",
+              record: filterRecords(records),
+              count: ret.length
+            });
+          })
+      }
+    })
+  } else {
+    models.knex.raw("SELECT keywords_records.record_id \
+      FROM keywords_records INNER JOIN keywords ON keywords_records.keyword_id = keywords.id \
+      WHERE keywords.id = ? \
+      GROUP BY keywords_records.record_id;", [query])
+    .then(function (ret){
+      ret = ret.rows
+      if(!ret) {
+        return res.json({
+          status: "success",
+          record: [],
+          count: 0
+        });
+      } else {
+        ret.forEach(function (record, index, records) {
+          records[index] = record["record_id"];
+        })
+        models.Record.query(function (qb) {
+            qb.where('id', 'in', ret).limit(limit).offset(offset)
+          })
+          .fetchAll({withRelated: ['category', 'carrier', 'collector', 'issue', 'language', 'pattern', 'keywords']})
+          .then(function (records){
+            return res.json({
+              status: "success",
+              record: filterRecords(records),
+              count: ret.length
+            });
+          })
+      }
+    })
+  }
+});
 
 module.exports = api;
